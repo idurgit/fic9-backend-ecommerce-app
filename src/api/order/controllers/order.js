9a2698f181435d700e37b99c25771c8c61f94ba8
Rudi <126459042+idurgit@users.noreply.github.com>
2023-10-20 16:15:41 +0700

    02-api-order

---
+++ src/api/order/controllers/order.js
@@ -0,0 +1,36 @@
+"use strict";
+
+/**
+ * order controller
+ */
+
+const { createCoreController } = require("@strapi/strapi").factories;
+
+module.exports = createCoreController("api::order.order", ({ strapi }) => ({
+  async create(ctx) {
+    const result = await super.create(ctx);
+
+    console.log("result", result);
+
+    const { default: axios } = require("axios");
+    const { xenditHeaders } = require("../helpers/header.js");
+
+    const payload = {
+      external_id: result.data.id.toString,
+      payer_email: "codewithbahri@gmail.com",
+      description: "Payment for product",
+      amount: result.data.attibutes.totalPrice,
+    };
+
+    const response = await axios({
+      method: "POST",
+      url: "https://api.xendit.co/v2/invoices",
+      headers: xenditHeaders,
+      data: JSON.stringify(payload),
+    });
+
+    console.log("response", response.data);
+
+    return JSON.stringify(response.data);
+  },
+}));
